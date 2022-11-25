739d27f23bc8d051845ecf6b15e424e8c69c46e9
NicolasPalia <alberto.nicolas.palia@gmail.com>
2022-11-25 18:34:39 -0300

    Contrellers

--- api/src/Controllers/index.js
+++ api/src/Controllers/index.js
@@ -4,20 +4,39 @@ const { Sequelize } = require("sequelize");
 const { Dog, Temperament } = require("../db");
 
 const infoApi = async () => {
-  const infoUrl  = await axios.get(URL);
-  const arrayDogs = infoUrl.data?.map((d)=>{
+  const infoUrl = await axios.get(URL);
+  const arrayDogs = infoUrl.data?.map((d) => {
     return {
-        id : d.id ,
-        name : d.name,
-        img: d.image.url ,
-        height : d.height.metric,
-        weight : d.weight.metric,
-        temperament: d.temperament,
-        life_span: d.life_span,
+      id: d.id,
+      name: d.name,
+      img: d.image.url,
+      height: d.height.metric,
+      weight: d.weight.metric,
+      temperament: d.temperament,
+      life_span: d.life_span,
+    };
+  });
 
-    }
-  })
-  return arrayDogs
+  return arrayDogs;
+};
+const getDogName = async (name) => {
+  const infoUrl = await axios.get(`${URL}/search?q=${name}`);
+  console.log(infoUrl);
+  return {
+    id: infoUrl.data[0].id,
+    name: infoUrl.data[0].name,
+    img: `https://cdn2.thedogapi.com/images/${infoUrl.data[0].reference_image_id}.jpg`,
+    height: infoUrl.data[0].height.metric,
+    weight: infoUrl.data[0].weight.metric,
+    temperament: infoUrl.data[0].temperament,
+    life_span: infoUrl.data[0].life_span,
+  };
 };
 
-module.exports={infoApi}
+const getId = async (id) => {
+  const dogs = await infoApi();
+  const dogFilter = dogs.filter((d) => d.id === parseInt(id));
+  return dogFilter[0];
+};
+
+module.exports = { infoApi, getDogName, getId };
--- api/src/routes/dogs.js
+++ api/src/routes/dogs.js
@@ -1,15 +1,50 @@
 const { Router } = require("express");
 const { Temperament, Dog } = require("../db");
-const { infoApi } = require("../Controllers/index");
+const { infoApi, getDogName, getId } = require("../Controllers/index");
 const router = Router();
 
+// [ ] GET /dogs:
+// Obtener un listado de las razas de perro
+// Debe devolver solo los datos necesarios para la ruta principal
+
+// [ ] GET /dogs?name="...":
+// Obtener un listado de las razas de perro que contengan la palabra ingresada como query parameter
+// Si no existe ninguna raza de perro mostrar un mensaje adecuado
+
 router.get("/", async (req, res) => {
+  const { name } = req.query;
+
   try {
-    const dogs = await infoApi();
-    res.status(200).send(dogs);
+    if (name) {
+      const dog = await getDogName(name);
+      res.status(200).send(dog);
+    } else {
+      const dogs = await infoApi();
+      res.status(200).send(dogs);
+    }
   } catch (err) {
     res.status(400).send(err);
   }
 });
+
+// [ ] GET /dogs/{idRaza}:
+// Obtener el detalle de una raza de perro en particular
+// Debe traer solo los datos pedidos en la ruta de detalle de raza de perro
+// Incluir los temperamentos asociados
+
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const dog = await getId(id);
+    res.status(200).send(dog);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
+// [ ] POST /dogs:
+// Recibe los datos recolectados desde el formulario controlado de la ruta de creación de raza de perro por body
+// Crea una raza de perro en la base de datos relacionada con sus temperamentos
 
 module.exports = router;
